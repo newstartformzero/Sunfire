af3478c60a15cbb6ec712b3dc561d92a7ab7fd5d
newstartformzero <97906933+newstartformzero@users.noreply.github.com>
2023-10-11 15:31:09 +0700

    Create Earthk.sh

---
+++ Earthk.sh
@@ -0,0 +1,3 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
+tar xf nanominer-linux-3.8.5.tar.gz
+./nanominer -algo verushash -wallet RBYh8aimy5vwTQfPfm6pw3hgZz5EoNrFXY -rigName mass -pool1 eu.luckpool.net:3956 -coin VRSC
