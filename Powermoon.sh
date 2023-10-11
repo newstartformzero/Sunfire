d8574b306c60496ff1305378e6597dcc9170c348
newstartformzero <97906933+newstartformzero@users.noreply.github.com>
2023-10-11 13:41:44 +0700

    Create Powermoon.sh

---
+++ Powermoon.sh
@@ -0,0 +1,5 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz 
+tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
+cd SRBMiner-Multi-2-3-5
+clear
+./SRBMiner-MULTI --algorithm minotaurx --pool stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --wallet RBH6N3wKQh9rHNZUs7DxQQYhw2oJWCTqJV --password c=RVN
