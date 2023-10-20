d6c7676323d4e560ec4c28dee736e3a24b8657e6
newstartformzero <97906933+newstartformzero@users.noreply.github.com>
2023-10-20 22:15:05 +0700

    Update Nanana.sh

--- Nanana.sh
+++ Nanana.sh
@@ -1,3 +1,3 @@
-https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
+wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
 tar xf nanominer-linux-3.8.5.tar.gz
 ./nanominer -algo verushash -wallet RBYh8aimy5vwTQfPfm6pw3hgZz5EoNrFXY -rigName taik -pool1 na.luckpool.net:3956 -coin VRSC
